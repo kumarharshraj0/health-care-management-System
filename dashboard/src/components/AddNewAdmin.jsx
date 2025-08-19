d0129cb35bec0eb270f6507556d797f064d6e33b
kumarharshraj0 <harshsawarn2@gmail.com>
2025-08-19 15:53:35 +0530

    Update AddNewAdmin.jsx

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://health-care-management-system-backend-l7jp.onrender.com/api/v1/user/login/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone,  dob, gender, password },
           {
             withCredentials: true,
