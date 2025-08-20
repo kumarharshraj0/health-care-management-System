6b683b9f39c0300b1a4b7dd7d1a06900efb3cb4b
kumarharshraj0 <harshsawarn2@gmail.com>
2025-08-20 12:30:00 +0530

    Update Sidebar.jsx

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("https://health-care-management-system-backend-l7jp.onrender.com/api/v1/user/login/api/v1/user/admin/logout", {
+      .get("http://localhost:4000/api/v1/user/admin/logout", {
         withCredentials: true,
       })
       .then((res) => {
