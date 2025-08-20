405d3bdc673737b042cc17218850d4575877b89c
kumarharshraj0 <harshsawarn2@gmail.com>
2025-08-20 12:08:18 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "https://health-care-management-system-backend-l7jp.onrender.com/api/v1/user/login",
+          "http://localhost:4000/api/v1/user/login",
           { email, password, role: "Patient" },
           {
             withCredentials: true,
